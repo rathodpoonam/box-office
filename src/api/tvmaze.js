beaf8eb76322b9786b1b2110ccb428777a7d483c
Tushar-Deshmukh <tushardeshmukh57985@gmail.com>
2023-12-03 07:59:40 +0000

    fetched and displayed data from tvmaze api created reusable function to show data

---
+++ src/api/tvmaze.js
@@ -0,0 +1,9 @@
+const baseUrl = 'https://api.tvmaze.com';
+const apiGet = async queryString => {
+  const response = await fetch(`${baseUrl}${queryString}`);
+  const body = await response.json();
+
+  return body;
+};
+
+export const searchForShows = query => apiGet(`/search/shows?q=${query}`);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-
+import { searchForShows } from '../api/tvmaze';
 const Home = () => {
   const [searchStr, setSearchStr] = useState('');
+  const [apiData, setApiData] = useState([]);
+  const [apiDataError, setApiDataError] = useState(null);
 
   const onSearchInputChnage = e => {
     setSearchStr(e.target.value);
@@ -9,22 +11,35 @@ const Home = () => {
 
   const onSearch = async e => {
     e.preventDefault();
-
-    const response = await fetch(
-      `https://api.tvmaze.com/search/shows?q=${searchStr}`
-    );
-
-    const body = await response.json();
-
-    console.log(body);
+    try {
+      setApiDataError(null);
+      const result = await searchForShows(searchStr);
+      setApiData(result);
+    } catch (error) {
+      setApiDataError(error);
+    }
   };
 
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <div>Error Occurred: {apiDataError.message}</div>;
+    }
+    if (apiData) {
+      return apiData.map(data => {
+        return <div key={data.show.id}> {data.show.name}</div>;
+      });
+    }
+
+    return null;
+  };
   return (
     <div>
       <form onSubmit={onSearch}>
         <input type="text" value={searchStr} onChange={onSearchInputChnage} />
         <button type="submit">Search</button>
       </form>
+
+      <div>{renderApiData()}</div>
     </div>
   );
 };
