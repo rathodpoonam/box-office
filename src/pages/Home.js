182fa2f8fa51e585167fd81216a3b14301bda05c
Tushar-Deshmukh <tushardeshmukh57985@gmail.com>
2023-12-03 06:32:30 +0000

    added an input in the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
-import React from 'react';
-
-
+import React, { useState } from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [inputValue, setInputValue] = useState('');
+  const onInputChnage = e => {
+    setInputValue(e.target.value);
+  };
+  return (
+    <div>
+      <input type="text" value={inputValue} onChange={onInputChnage} />
+    </div>
+  );
 };
 
 export default Home;
