ca4e7d563ac27f8b6329d263a9138b5177217a9e
Tushar-Deshmukh <tushardeshmukh57985@gmail.com>
2023-12-03 05:41:14 +0000

    added react-router-dom package

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/starred" element={<Starred />} />
+
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+<Link to="/starred">Go To Starred Page</Link>
+    </div>
+  )
+}
+
+export default Home
