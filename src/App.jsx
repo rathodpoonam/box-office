9309741fd9a98f56b8b3de3d7017806b1d93782e
Tushar-Deshmukh <tushardeshmukh57985@gmail.com>
2023-12-05 16:40:50 +0000

    deployed our app on github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Starred from './pages/Starred';
-import MainLayout from './components/MainLayout';
-import Show from './pages/Show';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { GlobalTheme } from './theme';
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Starred from "./pages/Starred";
+import MainLayout from "./components/MainLayout";
+import Show from "./pages/Show";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { GlobalTheme } from "./theme";
 
 function App() {
   const queryClient = new QueryClient();
@@ -14,7 +14,7 @@ function App() {
     <div>
       <QueryClientProvider client={queryClient}>
         <GlobalTheme>
-          <BrowserRouter>
+          <HashRouter>
             <Routes>
               <Route element={<MainLayout />}>
                 <Route path="/" element={<Home />} />
@@ -25,7 +25,7 @@ function App() {
 
               <Route path="*" element={<h1>Not Found</h1>} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </GlobalTheme>
       </QueryClientProvider>
     </div>
